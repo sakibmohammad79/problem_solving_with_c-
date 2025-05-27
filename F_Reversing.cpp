1d549af6303d8323c7b0f24bc2de0e2fef2a2644
sakib <mohammadsakib7679@gmail.com>
2025-05-27 22:14:26 +0600

    solve reverse array problem using swap fn

--- F_Reversing.cpp
+++ F_Reversing.cpp
@@ -1,17 +1,36 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n; 
+//     cin >> n;
+//     int digit[n];
+//     for(int i = 0; i<n; i++){
+//         cin >> digit[i];
+//     }
+//     for(int i = 0, j = n-1; i<j; i++, j--){
+//         int temp = digit[i];
+//         digit[i] = digit[j];
+//         digit[j] = temp;
+//     }
+//     for(int i = 0; i<n; i++){
+//         cout << digit[i] << " ";
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n; 
-    cin >> n;
+    int n; cin>>n;
     int digit[n];
     for(int i = 0; i<n; i++){
         cin >> digit[i];
     }
+
     for(int i = 0, j = n-1; i<j; i++, j--){
-        int temp = digit[i];
-        digit[i] = digit[j];
-        digit[j] = temp;
+        swap(digit[i], digit[j]);
     }
     for(int i = 0; i<n; i++){
         cout << digit[i] << " ";
