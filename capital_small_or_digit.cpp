b7276210e1895e5d1bcfc553fc1a81a792233277
sakib <mohammadsakib7679@gmail.com>
2025-05-27 20:54:04 +0600

    first commit

---
+++ capital_small_or_digit.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char ch;
+    cin>> ch;
+
+    if(ch >= 'a' && ch <='z'){
+        cout<< "ALPHA" << endl << "IS SMALL";
+    }
+    else if(ch >= 'A' && ch <='Z'){
+        cout<< "ALPHA" << endl << "IS CAPITAL";
+    }
+    else if(ch >= '0' && ch <='9'){
+        cout<< "IS DIGIT" << endl;
+    }
+    
+    return 0;
+}
