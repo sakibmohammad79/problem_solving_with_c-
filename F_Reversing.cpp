1c6e1b987446c39a46990607e861affc504084dd
sakib <mohammadsakib7679@gmail.com>
2025-05-27 22:08:06 +0600

    solved reversing problem

---
+++ F_Reversing.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; 
+    cin >> n;
+    int digit[n];
+    for(int i = 0; i<n; i++){
+        cin >> digit[i];
+    }
+    for(int i = 0, j = n-1; i<j; i++, j--){
+        int temp = digit[i];
+        digit[i] = digit[j];
+        digit[j] = temp;
+    }
+    for(int i = 0; i<n; i++){
+        cout << digit[i] << " ";
+    }
+    return 0;
+}
